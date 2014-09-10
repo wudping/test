d62a2a60e2853bb35d55d65cca7840838822d866
hidog <hidog.gao@msa.hinet.net>
2014-09-10 20:42:44 +0800

    move files

---
+++ SQLite_Test/src/SQLiteControl.cpp
@@ -0,0 +1,9 @@
+//
+//  SQLiteControl.cpp
+//  SQLite_Test
+//
+//  Created by hidog on 2014/9/10.
+//  Copyright (c) 2014年 hidog. All rights reserved.
+//
+
+#include "SQLiteControl.h"
---
+++ SQLite_Test/src/SQLiteControl.h
@@ -0,0 +1,14 @@
+//
+//  SQLiteControl.h
+//  SQLite_Test
+//
+//  Created by hidog on 2014/9/10.
+//  Copyright (c) 2014年 hidog. All rights reserved.
+//
+
+#ifndef SQLite_Test_SQLiteControl_h
+#define SQLite_Test_SQLiteControl_h
+
+
+
+#endif
