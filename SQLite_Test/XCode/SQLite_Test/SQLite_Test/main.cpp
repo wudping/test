734714729bc321f70a0730557662ff9a94bc8167
hidog <hidog.gao@msa.hinet.net>
2014-09-10 20:38:08 +0800

    add Xcode proj file

---
+++ SQLite_Test/XCode/SQLite_Test/SQLite_Test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SQLite_Test
+//
+//  Created by hidog on 2014/9/10.
+//  Copyright (c) 2014年 hidog. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
