6b92d0a08022c7a0cfdb663b7c233132d674a111
hidog <hidog.gao@msa.hinet.net>
2014-09-19 23:26:02 +0800

    add netlib test

--- netlib_test/src/server.hpp
+++ netlib_test/src/server.hpp
@@ -4,7 +4,7 @@
 
 #include <boost/network/protocol/http/server.hpp>
 #include <iostream>
-
+#include <stdio.h>
 
 namespace http = boost::network::http;
 
@@ -19,16 +19,26 @@ struct hello_world {
     /*<< This is the function that handles the incoming request. >>*/
     void operator() (server::request const &request,
                      server::response &response) {
+
+
         server::string_type ip = source(request);
         std::ostringstream data;
+
+		
         data << "Hello, " << ip << "!";
         response = server::response::stock_reply(
             server::response::ok, data.str());
+
+		std::cout << "test for wait " ;
+		for( int i = 0; i < 100000; i++ )
+			std::cout << "*";
+
     }
     /*<< It's necessary to define a log function, but it's ignored in
          this example. >>*/
-    void log(...) {
-        // do nothing
+    void log(/*http_server::string_type const &info*/...) {
+		std::cout << "test";
+		//std::cerr << "ERROR: " << info << '\n';
     }
 };
 
@@ -37,13 +47,8 @@ struct hello_world {
 
 
 
-int f(int argc, char * argv[]) {
+int server_test() {
     
-    /*if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " address port" << std::endl;
-        return 1;
-    }*/
-
     try {
         /*<< Creates the request handler. >>*/
         hello_world handler;
@@ -52,6 +57,8 @@ int f(int argc, char * argv[]) {
         server server_( "0.0.0.0", "8000", handler);
         /*<< Runs the server. >>*/
         server_.run();
+
+		//server_.log("test");
     }
     catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
