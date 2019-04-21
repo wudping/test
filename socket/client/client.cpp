8925351c18e2b2a71771134ebf51e9ae6f92606e
hidog <hidog.gao@msa.hinet.net>
2019-04-21 19:44:40 +0800

    add code

--- socket/client/client.cpp
+++ socket/client/client.cpp
@@ -99,6 +99,46 @@ void	f(int thr_id )
 
 
 
+int TCP_client()
+{
+	WORD sockVersion = MAKEWORD(2,2);
+	WSADATA data; 
+	if(WSAStartup(sockVersion, &data) != 0)
+	    return 0;
+	
+	SOCKET sclient = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if(sclient == INVALID_SOCKET)
+	{
+	    printf("invalid socket !");
+	    return 0;
+	}
+	
+	sockaddr_in serAddr;
+	serAddr.sin_family = AF_INET;
+	serAddr.sin_port = htons(12345);
+	serAddr.sin_addr.S_un.S_addr = inet_addr("220.135.38.106"); 
+	if (connect(sclient, (sockaddr *)&serAddr, sizeof(serAddr)) == SOCKET_ERROR)
+	{
+	    printf("connect error !");
+	    closesocket(sclient);
+	    return 0;
+	}
+	char * sendData = "hi i am client!\n";
+	send(sclient, sendData, strlen(sendData), 0);
+	
+	char recData[255];
+	int ret = recv(sclient, recData, 255, 0);
+	if(ret > 0)
+	{
+	    recData[ret] = 0x00;
+	    printf(recData);
+	}
+	closesocket(sclient);
+	WSACleanup();
+	return 0;
+}
+
+
 int UDP_send()
 {
 	WORD socketVersion = MAKEWORD(2,2);
@@ -133,7 +173,8 @@ int UDP_send()
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	UDP_send();
+	TCP_client();
+	//UDP_send();
 	//g(0);
 
 	/*boost::thread	thr[1];
--- socket/vs2013/vs2012.cpp
+++ socket/vs2013/vs2012.cpp
@@ -156,9 +156,77 @@ int UDP_send()
 
 
 
+int TCP_server()
+{
+	WORD sockVersion = MAKEWORD(2,2);
+	WSADATA wsaData;
+	if(WSAStartup(sockVersion, &wsaData) != 0)
+	    return 0;
+	
+	//
+	SOCKET skt = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if(skt == INVALID_SOCKET)
+	{
+	    printf("socket error !");
+	    return 0;
+	}
+	
+	//
+	sockaddr_in sin;
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(12345);
+	sin.sin_addr.S_un.S_addr = INADDR_ANY; 
+	if(bind(skt, (LPSOCKADDR)&sin, sizeof(sin)) == SOCKET_ERROR)
+	    printf("bind error !");
+	
+	//
+	if(listen(skt, SOMAXCONN) == SOCKET_ERROR)
+	{
+	    printf("listen error !");
+	    return 0;
+	}
+	
+	//
+	SOCKET sClient;
+	sockaddr_in remoteAddr;
+	int nAddrlen = sizeof(remoteAddr);
+	char revData[255]; 
+	while (true)
+	{
+	    printf("wait connect...\n");
+	    sClient = accept(skt, (SOCKADDR *)&remoteAddr, &nAddrlen);
+	    if(sClient == INVALID_SOCKET)
+	    {
+	        printf("accept error !");
+	        continue;
+	    }
+	    printf("get connect：%s \r\n", inet_ntoa(remoteAddr.sin_addr));
+	    
+	    //接收数据
+	    int ret = recv(sClient, revData, 255, 0);        
+	    if(ret > 0)
+	    {
+	        revData[ret] = 0x00;
+	        printf(revData);
+	    }
+	
+	    //发送数据
+	    char * sendData = "hi i am server！\n";
+	    send(sClient, sendData, strlen(sendData), 0);
+	    closesocket(sClient);
+	}
+	
+	closesocket(skt);
+	WSACleanup();
+	return 0;
+}
+
+
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	UDP_send();
+	TCP_server();
+	//UDP_send();
 	//f();
 
 	system("PAUSE");
